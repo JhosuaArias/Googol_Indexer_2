82902dd18063117ed7a54f2196278d04ff125a12
JhosuaArias <jhosuarqui@hotmail.com>
2018-10-08 21:03:54 -0600

    Initial commit

---
+++ DataModel.py
@@ -0,0 +1,23 @@
+class TokData:
+    def __init__(self):
+        pass
+
+
+class VocabularyData:
+    def __init__(self):
+        pass
+
+
+class WtdData:
+    def __init__(self):
+        pass
+
+
+class PostData:
+    def __init__(self):
+        pass
+
+
+class IndexData:
+    def __init__(self):
+        pass
---
+++ Functionality.py
@@ -0,0 +1,19 @@
+# Weights Files .wtd
+def calculateWeights():
+    pass
+
+
+def postTerm():
+    pass
+
+
+def indexTerm():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
